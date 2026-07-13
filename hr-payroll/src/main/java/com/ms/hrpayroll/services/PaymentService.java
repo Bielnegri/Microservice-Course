e55517e374a1c95275097dc09aeabec15dc40e37
Bielnegri <gabrielbnegri@gmail.com>
2026-07-13 16:43:53 -0300

    :sparkles: feat: Resilience4J and OpenFeign fallback and timeout configuration

--- hr-payroll/src/main/java/com/ms/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/ms/hrpayroll/services/PaymentService.java
@@ -3,6 +3,8 @@ package com.ms.hrpayroll.services;
 import com.ms.hrpayroll.entities.Payment;
 import com.ms.hrpayroll.entities.Worker;
 import com.ms.hrpayroll.feignclients.WorkerFeignClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +15,13 @@ public class PaymentService {
     @Autowired
     private WorkerFeignClient workerFeignClient;
 
+    @CircuitBreaker(name = "payment", fallbackMethod = "getPaymentAlternative")
     public Payment getPayment(Long workerId, Integer days){
         Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
+    }
+
+    public Payment getPaymentAlternative(Long workerId, Integer days,  Throwable e){
+        return new Payment("Brann", 400.0, days);
     }
 }
