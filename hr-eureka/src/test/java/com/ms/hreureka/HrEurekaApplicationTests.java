efd17df48a7bd639852092fcd5b8d53800b8865a
Bielnegri <gabrielbnegri@gmail.com>
2026-07-13 15:22:30 -0300

    :rocket: Project hr-eureka created

---
+++ hr-eureka/src/test/java/com/ms/hreureka/HrEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.hreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
